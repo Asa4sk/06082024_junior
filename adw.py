e8c1ba4f66b77375c3adc5ac18ffa4dcd5e2b836
Asus <nsa@gmail.com>
2024-11-06 22:18:44 +0200

    Homework

---
+++ adw.py
@@ -0,0 +1,19 @@
+def check_and_add_10(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        if type(result) is int:
+            new_result = {result + 10}
+        else:
+            new_result = {result}
+        return new_result
+
+    return wrapper
+
+
+@check_and_add_10
+def get_num(num):
+    return num
+
+
+number = 73
+print(get_num(num=number))
